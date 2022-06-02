c90f1696c80821f1820541c290f9075f05bcf54c
AFaioes <97889650+AFaioes@users.noreply.github.com>
2022-06-02 16:27:26 +0100

    Subheader created
    Subheader created, but needs to be updated

---
+++ src/components/common/SubHeader/index.tsx
@@ -0,0 +1,70 @@
+import { Button, Dropdown, Menu } from "antd";
+import React from "react";
+import Pen from "../../../images/pen.svg";
+
+
+const SubHeader = () => {
+
+    const menu = ( 
+        <Menu>
+      <Menu.Item key="1">Canetas de Metal</Menu.Item>
+      <Menu.Item key="2">Canetas de Escritório</Menu.Item>
+      <Menu.Item key="3">Conjuntos de Escrita</Menu.Item>
+        <Menu.Item key="4">Esferográficas</Menu.Item>
+        <Menu.Item key="5">Lápis e Lapiseiras</Menu.Item>
+        <Menu.Item key="6">Acessórios de escrita</Menu.Item>
+    </Menu>
+  );
+
+  return (
+    <><div className="subheader">
+          <ul className="subheader-menu-main">
+            <li>              
+              <Dropdown overlay={menu}>
+            <Button>
+            <img src={Pen} alt="Pen logo" />
+              Escrita                             
+            </Button>
+              </Dropdown>
+            </li>
+            <li>
+            <Dropdown overlay={menu}>
+            <Button>
+            <img src={Pen} alt="Pen logo" />
+                Categoria Produtos 1 
+            </Button>
+              </Dropdown>
+            </li>
+            <li>
+            <Dropdown overlay={menu}>
+            <Button>
+            <img src={Pen} alt="Pen logo" />
+            Categoria Produtos 2 
+            </Button>
+              </Dropdown>
+            </li>
+            <li>
+            <Dropdown overlay={menu}>
+            <Button>
+            <img src={Pen} alt="Pen logo" />
+            Categoria Produtos 3 
+            </Button>
+              </Dropdown>
+            </li>
+            <li>
+              <Dropdown overlay={menu}>
+            <Button>
+            <img src={Pen} alt="Pen logo" />
+            Categoria Produtos 4 
+            </Button>
+              </Dropdown>
+            </li>
+              
+          </ul>
+      </div><ul className="subheader-menu-item">
+              
+          </ul></>
+  );
+};
+
+export default SubHeader;
