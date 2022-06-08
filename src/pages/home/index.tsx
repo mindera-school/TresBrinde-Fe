6b30cb208115910bf940e0f6c4ced0bd63923de2
Tomás <tomas.fons8@gmail.com>
2022-06-08 17:46:01 +0100

    product category subtitle

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -9,11 +9,18 @@ const Home = () => {
     <>
       <div className="category-homepage">
         <Title>Categorias de Produtos</Title>
+        <h3>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam,
+          purus sit amet luctus venenatis, lectus magna fringila,
+        </h3>
         <CategoriesHome />
       </div>
       <div className="print-homepage">
         <Title>Personalização</Title>
-        <h3>Personalize os seus artigos com impressões de qualidade, a preços competitivos e sem custos fixos.</h3>
+        <h3>
+          Personalize os seus artigos com impressões de qualidade, a preços
+          competitivos e sem custos fixos.
+        </h3>
         <PrintHome></PrintHome>
       </div>
     </>
