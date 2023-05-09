09e13aaa0d34772b47144818b8c4c50e0725678a
LeoHabs <117355219+LeoHabs@users.noreply.github.com>
2023-05-09 14:31:08 +0100

    start new app

--- src/hooks/useDocumentTitle.tsx
+++
@@ -1,9 +0,0 @@
-import { useLayoutEffect } from "react";
-
-const useDocumentTitle = (title = "Três Brinde - Loja Online de Publicidade") => {
-  useLayoutEffect(() => {
-    document.title = title;
-  }, [title]);
-};
-
-export default useDocumentTitle;
--- src/pages/admin/dashboard/index.tsx
+++
@@ -1,9 +0,0 @@
-import useDocumentTitle from "../../../hooks/useDocumentTitle";
-
-const Dashboard = () => {
-  useDocumentTitle("Três Brinde | Admin Panel");
-
-  return <div>Olá, Jorge</div>;
-};
-
-export default Dashboard;
