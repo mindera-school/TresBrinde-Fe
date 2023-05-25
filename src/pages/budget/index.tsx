158262ecb51c2494c2971cf95326a9d3308d2f86
Adan0k <42160712+Adan0k@users.noreply.github.com>
2023-05-25 17:13:56 +0100

    delete useless things

--- src/pages/budget/index.tsx
+++ src/pages/budget/index.tsx
@@ -58,97 +58,11 @@ const BudgetPage = () => {
     if (cartItems.length === 0) {
       history.push("/cart");
     }
-    // eslint-disable-next-line
-  }, [cartItems]);
+  }, []);
 
   return (
-    <div className="center">
-      <Title>Solicitar Orçamento</Title>
-      <List
-        style={{ minWidth: "100px", width: "50%", margin: "auto" }}
-        bordered
-        footer={
-          <div className="center" style={{ textAlign: "end" }}>
-            Preço total: {totalPrice}€
-          </div>
-        }
-        dataSource={cartItems}
-        renderItem={(item) => (
-          <List.Item>
-            <List.Item.Meta
-              avatar={<Avatar src={`${API_IMAGE}${item.image}`} />}
-              title={<h3>{item.productName}</h3>}
-              description={`Quantidade: ${item.quantity} Preço: ${item.price}€`}
-            />
-          </List.Item>
-        )}
-      />
-      <div style={{ minWidth: "100px", width: "60%", margin: "auto" }}>
-        <Divider></Divider>
-        <Title title="Dados Pessoais"></Title>
-
-        <Form
-          name="basic"
-          layout="vertical"
-          onFinish={onFinish}
-          // onFinishFailed={onFinishFailed}
-        >
-          <Form.Item
-            required
-            label="Nome e Apelido"
-            name="name"
-            initialValue={userInfo.user ? userInfo.user.name : ""}
-          >
-            <Input defaultValue={userInfo.user ? userInfo.user.name : ""} />
-          </Form.Item>
-          <Form.Item
-            label="E-mail"
-            name="email"
-            initialValue={userInfo.user ? userInfo.user.email : ""}
-            rules={[
-              { type: "email", required: true, message: "Email incorrecto" },
-            ]}
-          >
-            <Input defaultValue={userInfo.user ? userInfo.user.email : ""} />
-          </Form.Item>
-
-          <Form.Item
-            name="phone"
-            label="Phone Number"
-            rules={[
-              { required: true, message: "Insere o teu numero de telefone!" },
-            ]}
-          >
-            <Input addonBefore={prefixSelector} style={{ width: "100%" }} />
-          </Form.Item>
-          <Form.Item name="address" required label="Morada para Entrega">
-            <Input />
-          </Form.Item>
-          <Form.Item name="postal_code" required label="Codigo Postal">
-            <Input />
-          </Form.Item>
-
-          <Divider></Divider>
-
-          <Form.Item name="message" label="Mensagem">
-            <Input.TextArea />
-          </Form.Item>
-
-          <Divider></Divider>
-
-          <Form.Item>
-            <Col flex="auto">
-              <Button
-                style={{ width: "100%" }}
-                type="primary"
-                htmlType="submit"
-              >
-                Solicitar Orçamento
-              </Button>
-            </Col>
-          </Form.Item>
-        </Form>
-      </div>
+    <div>
+      <div></div>
     </div>
   );
 };
